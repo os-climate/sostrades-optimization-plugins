eab2c1b1fc26aba12667596971a1089898dc310c
perrotcap <antoine.perrot@capgemini.com>
2025-02-07 09:29:40 +0100

    fixed header

--- sostrades_optimization_plugins/models/test_class.py
+++ sostrades_optimization_plugins/models/test_class.py
@@ -1,5 +1,5 @@
 '''
-Copyright 2022 Airbus SAS
+Copyright 2025/02/06 Capgemini
 Modifications on 2023/06/14-2024/06/24 Copyright 2023 Capgemini
 
 Licensed under the Apache License, Version 2.0 (the "License");
